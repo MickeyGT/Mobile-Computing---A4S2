f759bad77a1c6dd13894103d5142fcc7ec9d2cb4
Mickey <gabi_turcu_1997@yahoo.com>
2019-04-11 16:55:03 +0200

    Fixed google maps activity, changed the marker, changed the wifi activity text.

--- MC/app/src/main/java/com/example/mc/MapsActivity.java
+++ MC/app/src/main/java/com/example/mc/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng facultate = new LatLng(44.308912, 23.834812);
+        mMap.addMarker(new MarkerOptions().position(facultate).title("Magnifica noastra facultate."));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(facultate));
     }
 }
