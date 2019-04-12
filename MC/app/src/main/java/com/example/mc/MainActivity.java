4b0f4e604de178278aecf617ad634cb939623cc5
Mickey <gabi_turcu_1997@yahoo.com>
2019-04-12 15:47:25 +0200

    Android cloud messaging notification.

--- MC/app/src/main/java/com/example/mc/MainActivity.java
+++ MC/app/src/main/java/com/example/mc/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.mc;
 
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,16 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Activity;
-import android.os.Bundle;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
